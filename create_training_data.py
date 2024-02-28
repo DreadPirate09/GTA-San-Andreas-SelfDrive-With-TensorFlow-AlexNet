6d09ee852f6aa7252d4b612cd4266bd50bc38756
DreadPirate09 <62398759+DreadPirate09@users.noreply.github.com>
2024-02-28 22:37:43 +0200

    add script for data collection

---
+++ create_training_data.py
@@ -0,0 +1,63 @@
+import numpy as np
+from grabscreen import grab_screen
+import cv2
+import time
+from getkeys import key_check
+import os
+
+file_name = 'training_data.npy'
+
+if os.path.isfile(file_name):
+    print('File exists, loading previous data!')
+    training_data = list(np.load(file_name, allow_pickle=True))
+else:
+    print('File does not exist, starting fresh!')
+    training_data = []
+
+def keys_to_output(keys):
+    '''
+    Convert keys to a ...multi-hot... array
+
+    [A,W,D] boolean values.
+    '''
+    output = [0,0,0]
+    
+    if 'A' in keys:
+        output[0] = 1
+    elif 'D' in keys:
+        output[2] = 1
+    else:
+        output[1] = 1
+    return output
+    
+
+def main():
+
+    for i in list(range(4))[::-1]:
+        print(i+1)
+        time.sleep(1)
+        
+    while(True):
+        # 800x600 windowed mode
+        print('getting the frame with number: '+str(len(training_data)))
+        screen = grab_screen(region=(0,40,800,640))
+        if screen is not None:
+            screen = cv2.cvtColor(screen, cv2.COLOR_BGR2GRAY)
+        else:
+            print("Error: Screen capture failed.")
+        last_time = time.time()
+        # resize to something a bit more acceptable for a CNN
+        screen = cv2.resize(screen, (80,60))
+        keys = key_check()
+        output = keys_to_output(keys)
+        training_data.append([screen,output])
+        
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            cv2.destroyAllWindows()
+            break
+
+        if len(training_data) % 500 == 0:
+            print(len(training_data))
+            np.save(file_name,training_data)
+
+main()
